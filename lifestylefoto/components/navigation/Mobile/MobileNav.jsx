f05bdb9573accff62ec12cee6caed70302ddf348
Tom Grayston <tomgrayston@gmail.com>
2022-10-20 15:41:59 +0100

    fixed BurgerMenu using useState

--- lifestylefoto/components/navigation/Mobile/MobileNav.jsx
+++ lifestylefoto/components/navigation/Mobile/MobileNav.jsx
@@ -1,16 +1,19 @@
 import { AiOutlineMenu } from 'react-icons/ai';
-import MobileMenu from './MobileMenu';
+import { useState } from "react";
+import { BurgerMenu } from './BurgerMenu';
 
 const MobileNav = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const props = "bg-red-500"
+
     return(
         <div className='flex items-center justify-between px-2 py-1 bg-stone-100 lg:hidden'>
             <h1 className="items-center pt-1 text-4xl text-black nav ">
                     LifestyleFoto
             </h1>
-            <button className="block px-1 py-1 text-3xl text-black bg-inherit focus:outline-none hover:bg-gray-200 group">
-                <AiOutlineMenu/>
-                <MobileMenu/>
-            </button>
+            <BurgerMenu/>
         </div>
     )
 }
