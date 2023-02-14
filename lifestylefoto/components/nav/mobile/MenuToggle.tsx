ccd8b09a7ee83ae1b8018c5f5e584257880e6981
Tom Grayston <tomgrayston@gmail.com>
2023-02-14 18:13:55 +0000

    refactored IconButton to tsx

--- lifestylefoto/components/nav/mobile/MenuToggle.tsx
+++ lifestylefoto/components/nav/mobile/MenuToggle.tsx
@@ -1,29 +1,25 @@
-"use client"
+"use client";
 
-import IconButton from '../common/IconButton'
-import Menu from '../mobile/Menu';
-import MenuOptions from '../common/MenuOptions';
-import { ReactNode, useState } from 'react';
-import { AiOutlineMenu } from 'react-icons/ai';
+import IconButton from "../common/IconButton";
+import Menu from "../mobile/Menu";
+import { ReactNode, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 interface Props {
-    children?: ReactNode
+    children?: ReactNode;
 }
 
-const MenuToggle = ({children}: Props) => {
-
+const MenuToggle = ({ children }: Props) => {
     const [open, setOpen] = useState(false);
 
     return (
         <>
-            <IconButton onClick={() => setOpen(true)}>
-                <AiOutlineMenu/>
-            </IconButton>
+            <IconButton onClick={() => setOpen(true)} icon={<AiOutlineMenu />} />
             <Menu open={open} setOpen={setOpen}>
                 {children}
             </Menu>
         </>
-    )
-}
+    );
+};
 
 export default MenuToggle;
